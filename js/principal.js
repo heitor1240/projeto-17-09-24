a612307978f56f5baea70af6ec710b7f348229e9
heitor1240 <111541276+heitor1240@users.noreply.github.com>
2024-09-24 11:24:08 -0300

    Atividade 2

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,11 @@
-var titulo = document.querySelector(".titulo");
+var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes = document.querySelectorAll('.paciente');
+for (let contador = 0 ; contador < pacientes.length; contador++){
+    var dadosPaciente = pacientes[contador];
+    var peso = dadosPaciente.querySelector('.info-peso').textContent;
+    var altura = dadosPaciente.querySelector('.info-altura').textContent;
+    var imc = peso/(altura*altura);
+    dadosPaciente.querySelector('.info-imc').textContent = imc;
+}
